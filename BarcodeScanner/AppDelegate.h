d4b4f7c775960d358914174454f8aa46060b0270
Sureshkumar Linganathan <sureshkumar.linganathan@impigertech.com>
2017-03-10 15:13:00 +0530

    Initial Commit

---
+++ BarcodeScanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarcodeScanner
+//
+//  Created by Sureshkumar Linganathan on 10/03/17.
+//  Copyright © 2017 Impiger Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BarcodeScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BarcodeScanner
+//
+//  Created by Sureshkumar Linganathan on 10/03/17.
+//  Copyright © 2017 Impiger Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
