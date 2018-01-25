429e6c7d97d8c714462942b119580c133981d321
Sureshkumar Linganathan <sureshkumarlinganathan@suresh-kumar.local>
2018-01-25 16:55:26 +0530

    Added logic for scan barcode.

--- BarcodeScanner/ViewController.m
+++ BarcodeScanner/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController ()<AVCaptureMetadataOutputObjectsDelegate>
+
+@property (strong, nonatomic) AVCaptureSession *session;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer *previewPlayer;
 
 @end
 
@@ -16,9 +20,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setupView];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)setupView
+{
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error = nil;
+    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+    self.session = [[AVCaptureSession alloc]init];
+    [_session addInput:deviceInput];
+    AVCaptureMetadataOutput *captureOutput = [[AVCaptureMetadataOutput alloc]init];
+    [self.session addOutput:captureOutput];
+    [captureOutput setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    NSArray *barCodeTypes = [[NSArray alloc]initWithObjects:AVMetadataObjectTypeQRCode,AVMetadataObjectTypeCode39Code,AVMetadataObjectTypeCode39Mod43Code,AVMetadataObjectTypeCode93Code,AVMetadataObjectTypeCode128Code,AVMetadataObjectTypeEAN8Code,AVMetadataObjectTypeEAN13Code,AVMetadataObjectTypeAztecCode,AVMetadataObjectTypePDF417Code,AVMetadataObjectTypeQRCode,AVMetadataObjectTypeDataMatrixCode, nil];
+    [captureOutput setMetadataObjectTypes:barCodeTypes];
+    _previewPlayer = [AVCaptureVideoPreviewLayer layerWithSession:_session];
+    [_previewPlayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    [_previewPlayer setFrame:self.view.frame];
+    [self.view.layer addSublayer:_previewPlayer];
+    [_session startRunning];
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection;
+{
+    if((metadataObjects == nil)||([metadataObjects count]==0))
+    {
+        printf("no barcode detect");
+        return;
+    }
+    AVMetadataMachineReadableCodeObject *object = [metadataObjects objectAtIndex:0];
+    NSLog(@"%@",object.type);
+    NSLog(@"%@",object.stringValue);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
